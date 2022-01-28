f9e7c2cb15dd0f2c9aed11a383c88bd061c95875
n-izawa <82441584+n-izawa@users.noreply.github.com>
2022-01-28 09:33:12 +0900

    Create embed_db_info_docker.sh

---
+++ tests/embed_db_info_docker.sh
@@ -0,0 +1,25 @@
+# Database Settings ----------
+DB_NAME=testdb
+DB_HOST=db_postgres
+DB_PORT=5432
+DB_USER=main_user
+DB_PASSWORD=password
+
+# example
+#DB_NAME=testdb
+#DB_HOST=db_postgres
+#DB_PORT=5432
+#DB_USER=main_user
+#DB_PASSWORD=password
+# ----------------------------
+
+TEMP_FILE=$(mktemp)
+
+cat $1 |
+sed -e "s/<|DB_NAME|>/${DB_NAME}/g" |
+sed -e "s/<|DB_HOST|>/${DB_HOST}/g" |
+sed -e "s/<|DB_PORT|>/${DB_PORT}/g" |
+sed -e "s/<|DB_USER|>/${DB_USER}/g" |
+sed -e "s/<|DB_PASSWORD|>/${DB_PASSWORD}/g" > ${TEMP_FILE}
+
+mv ${TEMP_FILE} $1
